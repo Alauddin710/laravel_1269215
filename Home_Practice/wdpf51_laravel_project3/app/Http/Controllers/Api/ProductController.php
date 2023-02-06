6828d19b5a300d27e4876c1371fb5ae24d141b0b
Alauddin710 <mdalauddinalo710@gmail.com>
2023-02-06 19:08:19 +0600

    laravel_api_postman_data_send

---
+++ Home_Practice/wdpf51_laravel_project3/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::orderBy('created_at', 'DESC')->take(5)->get();
+        return $products;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $validation = $request->validate([
+            'product_name' => 'required',
+            'product_details' => 'min:5|max:200',
+            'product_price' => 'required',
+            'product_category' => 'required',
+            'product_stock' => 'required',
+            'product_image' => 'mimes:png,jpg,pdf|max:2048',
+        ]);
+        return Product::create($input);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrfail($id);
+        $input = $request->all();
+        return $product->update($input);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrfail($id);
+        if ($product->delete()) {
+            return response()->json(['message' => 'Successfully Deleted']);
+        }
+    }
+}
