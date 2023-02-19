450ce3e9156a23edd4df53f53714dd418454bf3b
Alauddin710 <mdalauddinalo710@gmail.com>
2023-02-19 19:20:19 +0600

    laravel8_vue_aksate

---
+++ Class/laravel8vue_shop/resources/views/font_page.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.fontend')
+
+@section('products')
+
+<!-- All Categories Section Begin -->
+<section class="categories">
+    <div class="section-title">
+        <h1> All Product</h1>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="categories__slider owl-carousel">
+                @foreach ( $products as $product )
+                    
+               
+                <div class="col-lg-3">
+                    <div class="categories__item set-bg" data-setbg="{{$product->image_name}}">
+                        <h5><a href="#">{{$product->name}}</a></h5>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</section>
+<!-- All Categories Section End -->
+
+@endsection
