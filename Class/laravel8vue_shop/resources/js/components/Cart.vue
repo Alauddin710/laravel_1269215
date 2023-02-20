ca7df8b696e037bfaaa049bfa3b4835b8af13cf7
Alauddin710 <mdalauddinalo710@gmail.com>
2023-02-20 19:11:40 +0600

    Addtocart

--- Class/laravel8vue_shop/resources/js/components/Cart.vue
+++ Class/laravel8vue_shop/resources/js/components/Cart.vue
@@ -2,11 +2,24 @@
     {{ itemCount }}
 </template>
 <script>
+import axios from "axios";
+
 export default {
     data() {
         return {
-            itemCount: 0
+            itemCount: ""
         };
+    },
+    created() {
+        this.get_cart_items();
+    },
+    methods: {
+        async get_cart_items() {
+            await axios.post("/cart").then(function(response) {
+                this.itemCount = response.data.items;
+                console.log(response.data.items);
+            });
+        }
     }
 };
 </script>
